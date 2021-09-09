7c20d4246bb019f7735478626977f3f86d7dc82f
Mobashir Farhan <farhanmobashir007@gmail.com>
2021-09-10 02:00:43 +0530

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+// styles
+
+
+
+
+// markup
+const IndexPage = () => {
+  return (
+    <main>
+     <h1>Hello Gatsby</h1>
+    </main>
+  )
+}
+
+export default IndexPage
